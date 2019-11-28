09601915374ec84a438e0fa864611a1303c9cb52
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2019-11-28 07:57:08 +0300

    Исправлено ядро для вызова функции вывода строки

--- kernel.c
+++ kernel.c
@@ -1,10 +1,15 @@
 /*
 *  kernel.c
 */
+
+#include "library/string.h";
+
+char *vidptr = (char*)0xB8000;
+
 void kmain(void)
 {
 	const char *str = "Simple OS, version 0.0.1";
-	char *vidptr = (char*)0xb8000; 	// Помещаем в указатель адрес на первый байт видеопамяти
+	//char *vidptr = (char*)0xb8000; 	// Помещаем в указатель адрес на первый байт видеопамяти
 	unsigned int i = 0;
 	unsigned int j = 0;
 
@@ -18,13 +23,7 @@ void kmain(void)
 	j = 0;
 
 	/* Вывод наименования ОС и версии ядра */
-	while(str[j] != '\0') {
-		vidptr[i] = str[j];
-		/* Указатель на строку увеличиваем на 1, а указатель видеопамяти
-    		на 2 байта (символ + аттрибут цвета) */
-		vidptr[i+1] = 0x07;
-		++j;
-		i = i + 2;
-	}
+	print_string(vidptr, 0, 0, str);
+	
 	return;
 }
