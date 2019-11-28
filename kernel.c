c052d99b09dfb80d6569e386c341ea676674561b
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2019-11-28 06:34:47 +0300

    Исправлены комментарии и строка вывода

--- kernel.c
+++ kernel.c
@@ -3,28 +3,25 @@
 */
 void kmain(void)
 {
-	const char *str = "my first kernel";
-	char *vidptr = (char*)0xb8000; 	//video mem begins here.
+	const char *str = "Simple OS, version 0.0.1";
+	char *vidptr = (char*)0xb8000; 	// Помещаем в указатель адрес на первый байт видеопамяти
 	unsigned int i = 0;
 	unsigned int j = 0;
 
-	/* this loops clears the screen
-	* there are 25 lines each of 80 columns; each element takes 2 bytes */
+	/* Очистка экрана */
 	while(j < 80 * 25 * 2) {
-		/* blank character */
 		vidptr[j] = ' ';
-		/* attribute-byte - light grey on black screen */
 		vidptr[j+1] = 0x07; 		
 		j = j + 2;
 	}
 
 	j = 0;
 
-	/* this loop writes the string to video memory */
+	/* Вывод наименования ОС и версии ядра */
 	while(str[j] != '\0') {
-		/* the character's ascii */
 		vidptr[i] = str[j];
-		/* attribute-byte: give character black bg and light grey fg */
+		/* Указатель на строку увеличиваем на 1, а указатель видеопамяти
+    		на 2 байта (символ + аттрибут цвета) */
 		vidptr[i+1] = 0x07;
 		++j;
 		i = i + 2;
