9ee3c0a900080c67fc3f49d53743cf985c92ab1a
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2019-11-30 00:05:35 +0300

    Изменено ядро для работы с библиотечными функциями

--- kernel.c
+++ kernel.c
@@ -1,29 +1,17 @@
 /*
 *  kernel.c
 */
-
-#include "library/string.h";
-
-char *vidptr = (char*)0xB8000;
+#include "library/string.h"
 
 void kmain(void)
 {
 	const char *str = "Simple OS, version 0.0.1";
-	//char *vidptr = (char*)0xb8000; 	// Помещаем в указатель адрес на первый байт видеопамяти
-	unsigned int i = 0;
-	unsigned int j = 0;
 
 	/* Очистка экрана */
-	while(j < 80 * 25 * 2) {
-		vidptr[j] = ' ';
-		vidptr[j+1] = 0x07; 		
-		j = j + 2;
-	}
-
-	j = 0;
+	clear_screen();
 
 	/* Вывод наименования ОС и версии ядра */
-	print_string(vidptr, 0, 0, str);
+	print_string(0, 0, str);
 	
 	return;
 }
