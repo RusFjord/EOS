e94dc08dcdec448d1bed0d7ef65b721896ff2779
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2019-11-30 00:04:58 +0300

    Константы для стандартной библиотеки

---
+++ library/constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+/*
+*   Константы для библиотечного модуля string
+*/
+#define VIDEO_MEMORY_ADDRESS 0xB8000 
+#define SPACE_CHAR ' '
+#define BACKGROUND_COLOR_DEFALT 0x07  
+
+#endif
