2b8070f55545deaddd619ef265ab83f64fded016
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2020-04-26 10:37:08 +0300

    Новые константы и оформление для документации

--- library/constants.h
+++ library/constants.h
@@ -1,11 +1,35 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-/*
-*   Константы для библиотечного модуля string
-*/
+/**
+ * @brief Адрес видеопамяти
+ */
 #define VIDEO_MEMORY_ADDRESS 0xB8000 
+
+/**
+ * @brief Символ пробела
+ */
 #define SPACE_CHAR ' '
-#define BACKGROUND_COLOR_DEFALT 0x07  
+
+/**
+ * @brief Аттрибуты цвета для символа по умолчанию
+ */
+#define BACKGROUND_COLOR_DEFALT 0x07 
+
+/**
+ * @brief Максимальное количество строк в текстовом режиме
+ */
+#define MAX_ROWS 25
+
+/**
+ * @brief Максимальное количество колонок в текстовом режиме
+ */
+#define MAX_COLS 80
+
+/**
+ * @brief Порты ввода/вывода экрана
+ */
+#define REG_SCREEN_CTRL 0x3d4
+#define REG_SCREEN_DATA 0x3d5
 
 #endif
