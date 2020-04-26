3bc6b81d1712bc18750d31b3f4531a15f8bd7ac9
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2020-04-26 10:51:42 +0300

    Исправлены коментарии для документации

--- library/string.h
+++ library/string.h
@@ -1,12 +1,20 @@
 #ifndef STRING_H
 #define STRING_H
 
-/*
-*   Печать строки с началом в определенной строке и колонке
-*/
-void print_string(char col, char row, char *str);
+#include "types.h"
 
-void clear_screen();
+/**
+* @brief Вывод текста с началом в определенной строке и колонке
+* @param col колонка экрана начала вывода строки
+* @param row строка экрана начала вывода текста 
+* @param str указатель на си-подобную строку
+*/
+extern void print_string(uint8 col, uint8 row, char *str);
+
+/**
+* @brief Очистка экрана
+*/
+extern void clear_screen();
 
 #endif // STRING_H
 
