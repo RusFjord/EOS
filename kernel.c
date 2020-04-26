88f688ef8af3ad679476fe22a900f3dac7f17cbe
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2020-04-26 10:53:34 +0300

    Добавлен вывод курсора

--- kernel.c
+++ kernel.c
@@ -2,6 +2,7 @@
 *  kernel.c
 */
 #include "library/string.h"
+#include "library/ports.h"
 
 void kmain(void)
 {
@@ -12,6 +13,15 @@ void kmain(void)
 
 	/* Вывод наименования ОС и версии ядра */
 	print_string(0, 0, str);
+	
+	unsigned char cursor_start = 14;
+	unsigned char cursor_end = 15;
+
+	port_byte_out(0x3D4, 0x0A);
+	port_byte_out(0x3D5, (port_byte_in(0x3D5) & 0xC0) | cursor_start);
+	port_byte_out(0x3D4, 0x0B);
+	port_byte_out(0x3D5, (port_byte_in(0x3D5) & 0xE0) | cursor_end);
+	
 	
 	return;
 }
