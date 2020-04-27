acad9f90bfecfad0eb5d2379b80380219daa1240
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2020-04-27 07:09:07 +0300

    Исправлен формат комментариев для doxygen

--- library/screen.h
+++ library/screen.h
@@ -4,19 +4,19 @@
 #include "types.h"
 
 /**
-* @brief Инициализация экрана
+* \brief Инициализация экрана
 */
 extern void screen_init();
 
 /**
-* @brief Очистка экрана
+* \brief Очистка экрана
 */
 extern void clear_screen();
 
 /**
-* @brief Установка курсора
-* @param uint8 row - колонка курсора
-* @param uint8 col - строка курсора
+* \brief Установка курсора
+* \param uint8 row - колонка курсора
+* \param uint8 col - строка курсора
 */
 extern void set_cursor(uint8 row, uint8 col);
 
