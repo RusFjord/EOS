f3f649866cd6d8a3d96801bb8944713cdbd5069f
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2020-04-27 11:10:13 +0300

    Исправлены типы и выделена инициализация курсора

--- library/screen.c
+++ library/screen.c
@@ -1,19 +1,12 @@
 #include "screen.h"
 #include "ports.h"
 #include "constants.h"
+#include "types.h"
 
 void screen_init() {
 
     clear_screen();
-
-    unsigned char cursor_start = 14;
-	unsigned char cursor_end = 15;
-
-	outb(0x3D4, 0x0A);
-	outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_start);
-	outb(0x3D4, 0x0B);
-	outb(0x3D5, (inb(0x3D5) & 0xE0) | cursor_end);
-
+	init_cursor(14, 15);
     set_cursor(0, 0);
 }
 
@@ -21,11 +14,19 @@ void clear_screen() {
 
     uint8* video_memory = (uint8*)VIDEO_MEMORY_ADDRESS;
 	uint16 i = 0;
-	while(i < 80 * 25 * 2) {
+	while(i < MAX_COLS * MAX_ROWS * 2) {
 		video_memory[i] = SPACE_CHAR;
 		video_memory[i+1] = BACKGROUND_COLOR_DEFALT; 		
 		i = i + 2;
 	}
+}
+
+void init_cursor(uint8 cursor_start, uint8 cursor_end) {
+
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_start);
+	outb(0x3D4, 0x0B);
+	outb(0x3D5, (inb(0x3D5) & 0xE0) | cursor_end);
 }
 
 void set_cursor(uint8 row, uint8 col) {
