2a63e8c5e7f96e95c1af667141762867f80e029f
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2021-08-06 14:15:08 +0300

    Оформлены комментарии для документации

--- include/types.h
+++ include/types.h
@@ -1,10 +1,33 @@
+/**
+ * @file types.h
+ * @author Федор Ендовицкий (f.endovitskiy@ulteam8.ru)
+ * @brief Определение типов
+ * @version 0.1
+ * @date 06-08-2021
+ * 
+ */
 #ifndef TYPES_H
 #define TYPES_H
 
+/**
+ * @brief Беззнаковое 8-битное число
+ */
 typedef unsigned char uint8;
+/**
+ * @brief Беззнаковое 16-битное число
+ */
 typedef unsigned short uint16;
+/**
+ * @brief 8-битное число со знаком
+ */
 typedef char int8;
+/**
+ * @brief 16-битное число со знаком
+ */
 typedef short int16;
+/**
+ * @brief 32-битное число со знаком
+ */
 typedef unsigned int uint32;
 
-#endif
+#endif //TYPES_H
